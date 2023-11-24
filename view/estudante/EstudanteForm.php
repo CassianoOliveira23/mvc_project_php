859e61166cabb9f955418940d1a04659df0b11e3
CassianoOliveira23 <cassiano.borba@rede.ulbra.br>
2023-11-24 15:45:07 -0300

    add

--- view/estudante/EstudanteForm.php
+++ view/estudante/EstudanteForm.php
@@ -8,8 +8,7 @@
 </head>
 
 <body>
-  <header class="bg-black text-white p-5 text-center m-0">    
-  </header>
+  <header class="bg-black text-white p-5 text-center m-0"></header>
 
   <?php require_once  $_SERVER['DOCUMENT_ROOT'] . '/' . FOLDER . '/view/navbar.php'; ?>
 
@@ -17,24 +16,26 @@
 
 
     <div class="container mt-5">
-    <form method="POST" action="/<?php echo FOLDER; ?>/?controller=Estudante&acao=salvar">
-  <div class="mb-3">
-    <label for="nome" class="form-label">Name </label>
-    <input type="text" class="form-control" id="nome" name="nome">
-    
-  </div>
-  <div class="mb-3">
-    <label for="idade" class="form-label">Age</label>
-    <input type="number" class="form-control" id="idade" name="idade">
-  </div>
-  <button type="submit" class="btn btn-success">Save</button>
-    </form>
-
+      <form method="POST" action="/<?php echo FOLDER; ?>/?controller=Estudante&acao=salvar">
+    <div class="mb-3">
+      <label for="nome" class="form-label">Name </label>
+      <input type="text" class="form-control" id="nome" name="nome">
+      
     </div>
+    <div class="mb-3">
+      <label for="idade" class="form-label">Age</label>
+      <input type="number" class="form-control" id="idade" name="idade">
+    </div>
+    <button type="submit" class="btn btn-success">Save</button>
+      </form>
+    </div>
+
+
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 
+  
     
 </body>
 
